f3c1268759cfeba7fd475618d7e07d53d83aaebc
Marti <motimotmot21@gmail.com>
2025-04-08 00:46:27 +0800

    Added responsive content for the landing page

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+// Header functionalities
 
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
-
-    //Hamburger Animation
-    hamburger.classList.toggle("toggle");
+window.addEventListener("scroll", () => {
+  const bg = document.querySelector(".bg-img");
+  const offset = window.scrollY;
+  bg.style.transform = `translateY(${offset * 0.5}px)`; // adjust 0.5 for more/less parallax
 });
+
+
+//This ensures that when the page is reloaded or navigated to again, it scrolls to the very top.
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
+  document.querySelectorAll('.video-item').forEach(item => {
+    item.addEventListener('click', function () {
+      const videoId = this.getAttribute('data-video-id');
+
+      if (videoId) {
+        const mainIframe = document.querySelector('.video-main iframe');
+        mainIframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+      }
+    });
+  });
+
+
+ 
